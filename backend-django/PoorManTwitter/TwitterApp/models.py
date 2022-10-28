f095572bd61dc830a83ee2a263c2b11a0afdbf40
PeluMike <Pmichael3006@gmail.com>
2022-10-28 15:25:11 -0400

    more corrections

--- backend-django/PoorManTwitter/TwitterApp/models.py
+++ backend-django/PoorManTwitter/TwitterApp/models.py
@@ -7,8 +7,8 @@ import uuid
 
 
 class TwitterApp(models.Model):
-    name = models.CharField(max_length=300)
-    tweet = models.TextField()
+    name = models.CharField(max_length=300, blank=False, null=False)
+    tweet = models.CharField(max_length=50, blank=False, null=False)
     dateCreated = models.DateTimeField(auto_now_add= True)
     # id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     
